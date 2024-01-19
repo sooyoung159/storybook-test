d7064e9db61b51bbd3b6ab420a72f117850a9663
sooyoung <sooyoung159@naver.com>
2024-01-19 20:39:22 +0900

    스토리북 초기세팅 with react(vite)

---
+++ .storybook/main.ts
@@ -0,0 +1,33 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import {mergeConfig} from "vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['storybook-dark-mode'],
+      },
+    });
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
+
